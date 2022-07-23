46d39b37498b3aa52a132807d498e8e89f53e3aa
karoterra <karoterra2008@gmail.com>
2022-07-23 16:56:05 +0900

    :bookmark: chore: v0.3.0

--- src/show_limit.cpp
+++ src/show_limit.cpp
@@ -22,7 +22,7 @@ constexpr int kIdScriptSaveButton = 1102;
 constexpr int kIdList = 1900;
 
 const char* kFilterName = "上限確認";
-const char* kFilterInformation = "上限確認 v0.2.1 by karoterra";
+const char* kFilterInformation = "上限確認 v0.3.0 by karoterra";
 
 const FilterPlugin* g_filter = nullptr;
 
